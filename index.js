3f6e04839aed870206c4dcaeb75f07b3fe293334
Bryan Cauã Arruda Santos <bryanncaua@gmail.com>
2023-11-29 21:41:54 -0300

    completar tarefa no banco de dados, e tentativas de corrigir o erro de marcar recarregar a página e não continuar marcado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const mysql = require("mysql2")
 
+
 const app = express()
 
 app.engine("handlebars", exphbs.engine())
@@ -17,6 +18,42 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 //rotas
+app.post('/completar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPDATE tarefas 
+        SET completas = '1'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
+app.post('/descompletar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        UPTADE tarefas
+        SET completas = '0'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
@@ -24,7 +61,7 @@ app.post('/criar', (requisicao, resposta) => {
 
     const sql = `
         INSERT INTO tarefas(descricao, completas)
-        VALUES('${descricao}', '${completa}')
+        VALUES ('${descricao}', '${completa}')
     `
 
     conexao.query(sql, (erro) => {
@@ -41,7 +78,7 @@ app.post('/criar', (requisicao, resposta) => {
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
 
-    conexao.query(sql, (erro, dados) =>{
+    conexao.query(sql, (erro, dados) => {
         if (erro) {
             return console.log(erro)
         }
@@ -54,7 +91,7 @@ app.get('/', (requisicao, resposta) => {
             }
         })
 
-        resposta.render('home', {tarefas})
+        resposta.render('home', { tarefas })
     })
 })
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
+function completarTarefa(id) {
+    fetch("http://localhost:3000/completar", {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id })
+    })
+
+    window.location.reload()
+}
+
+function descompletarTarefa(id) {
+    fetch("http://localhost:3000/descompletar", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id })
+    })
+
+    window.location.reload()
+}
+
+
 function alterarTema() {
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
@@ -12,7 +37,7 @@ function alterarTema() {
             body.classList.add("dark")
         } else {
             novoTema = "light"
-            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de lua">`
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de Lua">`
             body.classList.remove("dark")
             body.classList.add("light")
         }
@@ -33,10 +58,10 @@ function verificarTema() {
     if (tema) {
         if (tema === "dark") {
             body.classList.add("dark")
-            `<img src="/imagens/sun-icon.png" alt="ícone de Sol">`
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="ícone de Sol">`
         } else {
             body.classList.add("light")
-            `<img src="/imagens/moon-icon.png" alt="ícone de lua">`
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de lua">`
         }
     }
 }
